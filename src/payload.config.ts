d31a7fa4716ba83ff878f5ef92faaefe9dded611
Rajneesh Kumar <rajneeshkumarit711@gmail.com>
2025-06-18 17:52:59 +0530

    added subscript and superscript

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,33 +10,9 @@ import sharp from 'sharp'
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { Posts } from './collections/Posts'
-import { MarkNode } from './lexical/nodes/MarkNode'
-import { MarkPlugin } from './lexical/plugins/MarkPlugin'
-import { MarkButton } from './lexical/components/MarkButton'
-import { FootnoteNode } from './lexical/nodes/FootnoteNode'
-import { FootnotePlugin } from './lexical/plugins/FootnotePlugin'
-import { FootnoteButton } from './lexical/components/FootnoteButton'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
-
-const markFeature = {
-  feature: {
-    nodes: [MarkNode],
-    plugins: [MarkPlugin],
-    Button: MarkButton,
-  },
-  key: 'mark',
-};
-
-const footnoteFeature = {
-  feature: {
-    nodes: [FootnoteNode],
-    plugins: [FootnotePlugin],
-    Button: FootnoteButton,
-  },
-  key: 'footnote',
-};
 
 export default buildConfig({
   admin: {
@@ -47,9 +23,9 @@ export default buildConfig({
   },
   collections: [Users, Media, Posts],
   editor: lexicalEditor({
-    features: ({ defaultFeatures }) => defaultFeatures.filter(
-      feature => !['subscript', 'superscript'].includes(feature.key)
-    ),
+    features: ({ defaultFeatures }) => [
+      ...defaultFeatures,
+    ],
   }),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
